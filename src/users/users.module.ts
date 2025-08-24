920b2404ba215866dce65cae32aa31b0ea90e403
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2025-08-24 08:27:45 +0300

    fix bug

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,15 @@
+import { User } from 'src/entities/user.entity';
+
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
